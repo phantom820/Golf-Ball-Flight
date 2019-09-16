3def699a8f54600b79b13b046f89137d714faca9
phantom820 <1611821@students.wits.ac.za>
2019-09-16 21:43:40 +0200

    second commit

--- Project/animation.py
+++ Project/animation.py
@@ -1,56 +1,103 @@
 from vpython import *
+import math
 
 #make the pause or start menu
 
 #animation starts out pause
 running=False
+stop=False
+ball=None
 
-def Run():
+#contols pausing
+def run():
 	global running
+	global stop
 	if(not running):
 		running=True
-	
+		stop=False
 	else:
 		running=False
+		
 
-#setting the launch speed
+#reset animation to default after changing speed or any reset event
+def reset(ball):
+	global running
+	global stop
+	running=False
+	stop=True
+	ball.pos=vector(0,0.1, 0)
+	
+#setting the launch speed resets things to default
 def launchSpeed(s):
 	wt.text='{:1.2f}'.format(s.value)
+	global ball
+	reset(ball)
 
 
-	#make the ball bounce up and down with some slider velocity
+#make the ball bounce up and down with some slider velocity
 def bounce(ball,v):
+	global stop
 	ball.velocity=vector(0,v,0)
 	ball.mass=0.25
-	
 	ball.p=ball.velocity*ball.mass
 	g=vector(0,-9.8,0)
 	Fnet=g*ball.mass
 	dt=0.001
 	t=0
 	
-	while running:
-		rate(300)
-		ball.pos=ball.pos + (ball.p/ball.mass)*dt
-		ball.p = ball.p +Fnet*dt
-		t = t + dt
-		if ball.pos.y < (floor.pos.y + ball.radius):
-			ball.p = -ball.p
+	while True:
+		if running:
+			rate(300)
+			ball.pos=ball.pos + (ball.p/ball.mass)*dt
+			ball.p = ball.p +Fnet*dt
+			t = t + dt
+			if ball.pos.y < (floor.pos.y + ball.radius):
+				ball.p = -ball.p
+	
+		elif stop:
+			break
+			
+#simulate projectile motion without air resistance basic one
+def simpleProjectile(ball,v):
+	global running
+	#say the angle is 30 degrees
+	vx=v*math.cos(math.radians(30))
+	vy=v*math.cos(math.radians(30))
+	ball.mass=0.25
+	
+	g=vector(0,-9.8,0)
+	ball.v=vector(vx,vy,0)
+	dt=0.001
+	t=0
+	
+	Fnet=g*ball.mass
 
+	while ball.pos.y>=0.1:
+		if running:
+			rate(300)
+			ball.v=ball.v+(Fnet/ball.mass)*dt
+			ball.pos=ball.pos+ball.v*dt/ball.mass
+			t=t+dt
+		
+		elif stop:
+			break
+					
 #set the title
 scene.title="Projectiles\n"
 	
-#make the scene dimensions
-'''for now default are fine '''
+#make the scene dimensions and properties
+scene.width=1000
+scene.height=800
 
 #make the pause button to control animation
 
-button(text="Pause",pos=scene.title_anchor,bind=Run) 
+button(text="Pause",pos=scene.title_anchor,bind=run) 
 
 #create ground and ball
 ball=sphere(pos=vector(0,0.1, 0), radius=0.1, color=color.white)
 floor=box(pos=vector(0,0,0), size=vector(1,0.05,1),
 color=color.green)
+
 
 #create initial velocity and angular launch angle slider
 
@@ -61,9 +108,13 @@ wt=wtext(text='{:1.2f}'.format(vsl.value))
 
 scene.append_to_caption(" metres/s \n")
 
-scene.camera.follow(ball)
+
+#main loop animation
+b=attach_trail(ball)
 
 while True:
-	print(running)
-	bounce(ball,4)
+	simpleProjectile(ball,1*vsl.value)
+	print(scene.camera.pos)
+	#b.clear()
+
 
