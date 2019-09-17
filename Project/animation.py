a7c9af4da85d1a9307fae9c575f1a8f794ffbc93
phantom820 <1611821@students.wits.ac.za>
2019-09-17 05:26:20 +0200

    added euler method for simple projectile now must incorporate air resistance

--- Project/animation.py
+++ Project/animation.py
@@ -1,8 +1,24 @@
 from vpython import *
 import math
+import random
 
+#numerical methods
+
+#the euler method t
+def euler(y0,yprime0,dt):
+	y=[y0]
+	yprime=[yprime0]
+	t=[0]
+	i=0;
+	while y[i]>=0:
+		y.append(y[i]+dt*yprime[i])
+		yprime.append(yprime[i]-9.8*dt) 
+		t.append(t[i]+dt)
+		i=i+1
+
+	return (y,yprime)
+	
 #make the pause or start menu
-
 #animation starts out pause and declare global variables
 running=False
 stop=False
@@ -27,7 +43,7 @@ def reset(ball):
 	global stop
 	running=False
 	stop=True
-	ball.pos=vector(0,0.1, 0)
+	ball.pos=vector(-60,0.1,0)
 	
 #setting the launch speed resets things to default
 def launchSpeed(s):
@@ -49,7 +65,7 @@ def bounce(ball,v):
 	
 	while True:
 		if running:
-			rate(300)
+			rate(100)
 			ball.pos=ball.pos + (ball.p/ball.mass)*dt
 			ball.p = ball.p +Fnet*dt
 			t = t + dt
@@ -67,26 +83,53 @@ def simpleProjectile(ball,v):
 	vx=v*math.cos(math.radians(30))
 	vy=v*math.cos(math.radians(30))
 	ball.mass=0.25
-	
+
 	g=vector(0,-9.8,0)
 	ball.v=vector(vx,vy,0)
 	dt=0.001
-	t=0
-	
+	t=0	
 	Fnet=g*ball.mass
 
 	while ball.pos.y>=0.1:
 		if running:
-			rate(300)
+			rate(350)
 			ball.v=ball.v+(Fnet/ball.mass)*dt
 			ball.pos=ball.pos+ball.v*dt/ball.mass
 			t=t+dt
 		
 		elif stop:
+			trail.clear()	
 			break
-	trail.clear()
+
 	print(ball.pos)
-	print(t)
+	
+def simpleNumericalProjectile(ball,v):
+	global running
+	global trail
+	#say the angle is 30 degrees
+	vx=v*math.cos(math.radians(30))
+	vy=v*math.cos(math.radians(30))
+	ball.mass=0.25
+	
+	#use euler method for y position
+	data=euler(0.1,vy,0.001)
+	y=data[0]
+	yprime=data[1]
+	i=0
+	t=0
+	dt=0.001
+	while ball.pos.y>=0.1:
+		if(running):
+			rate(350)
+			ball.v=vector(vx,yprime[i],0)
+			ball.pos=vector(vx*dt+ball.pos.x,y[i],0)
+			i=i+1
+			t=t+dt
+			
+		elif stop:
+			#trail.clear()	
+			break
+	print(ball.pos)		
 #set the title
 scene.title="Projectiles\n"
 	
@@ -94,16 +137,17 @@ scene.title="Projectiles\n"
 scene.width=1200
 scene.height=800
 scene.autoscale=False
-scene.camera.pos=vector(0,3,-10)
-scene.camera.rotate(-math.pi/2.4,vector(0,1.3,0),vector(0,0,0))
+scene.camera.pos=vector(0,3,50)
+scene.camera.rotate(-math.pi/2.02,vector(0,1.3,0),vector(0,0,0))
 scene.background=color.white
+
 
 #make the pause button to control animation
 
 button(text="Pause",pos=scene.title_anchor,bind=run) 
 
 #create ground and ball
-ball=sphere(pos=vector(0,0.1, 0), radius=0.1, color=color.white)
+ball=sphere(pos=vector(-60,0.1,0 ), radius=0.1, color=color.white)
 floor=box(pos=vector(5,0,0), size=vector(160,0.05,5),
 color=color.green)
 
@@ -118,15 +162,17 @@ wt=wtext(text='{:1.2f}'.format(vsl.value))
 scene.append_to_caption(" metres/s \n")
 
 
-
+#set the trail that follows
 trail=attach_trail(ball)
-trail.trail_radius=0.1
-trail.color=color.red
-
+trail.interval=20
+trail.color=color.cyan
 
 #main animation loop
 while True:
-	simpleProjectile(ball,vsl.value/2.7)
 	
+	if running:
+		simpleNumericalProjectile(ball,vsl.value/1.4)	
+		reset(ball)	
+		trail.clear()
 
 
